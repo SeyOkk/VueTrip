b2cc000f09bdfd08a8478fa072a8adbb7f459ef4
sqlee <1984563911@qq.com>
2023-11-19 20:52:34 +0800

    feat:首页日期选择

--- src/views/home/HomeSearch.vue
+++ src/views/home/HomeSearch.vue
@@ -2,6 +2,8 @@
 import { useRouter } from "vue-router";
 import useCityStore from "@/stores/modules/cityStore";
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
+import { formatMonth } from "@/utils/dateUtil";
 
 const router = useRouter();
 const positionClick = () => {
@@ -22,37 +24,89 @@ const gotoCity = () => {
 };
 
 const { currentCity } = storeToRefs(useCityStore());
+
+const nowDate = new Date();
+const nowDay = ref(formatMonth(nowDate));
+const nextDay = ref(formatMonth(nowDate.setDate(nowDate.getDate() + 1)));
 </script>
 
 <template>
-  <div class="location">
-    <div class="city" @click="gotoCity">{{ currentCity.cityName }}</div>
-    <div class="position" @click="positionClick">
-      <span>我的位置</span>
-      <img src="../../assets/img/home/icon_location.png" alt="" />
+  <div class="search">
+    <div class="location">
+      <div class="city" @click="gotoCity">{{ currentCity.cityName }}</div>
+      <div class="position" @click="positionClick">
+        <span>我的位置</span>
+        <img src="../../assets/img/home/icon_location.png" alt="" />
+      </div>
+    </div>
+
+    <div class="selection">
+      <div class="common">
+        <span class="text">入住</span>
+        <span class="date">{{ nowDay }}</span>
+      </div>
+
+      <div class="total-day">共一晚</div>
+
+      <div class="common">
+        <span class="text">离店</span>
+        <span class="date">{{ nextDay }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="less">
-.location {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 8px 12px 0 12px;
+.search {
+  padding: 0 10px;
 
-  .city {
-    font-size: 15px;
+  .location {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 8px 12px 0 12px;
+
+    .city {
+      font-size: 15px;
+    }
+
+    .position {
+      display: flex;
+      align-items: center;
+      font-size: 12px;
+
+      img {
+        width: 22px;
+        margin-left: 6px;
+      }
+    }
   }
 
-  .position {
+  .selection {
     display: flex;
+    justify-content: space-between;
     align-items: center;
-    font-size: 12px;
+    margin: 10px 10px;
+    width: 80%;
 
-    img {
-      width: 22px;
-      margin-left: 6px;
+    .common {
+      display: flex;
+      flex-direction: column;
+
+      .text {
+        font-size: 12px;
+        color: rgb(150, 147, 147);
+        margin-bottom: 4px;
+      }
+
+      .date {
+        color: #5d5a5a;
+      }
+    }
+
+    .total-day {
+      font-size: 13px;
+      color: #645d5d;
     }
   }
 }
